95243b3a54650f411b1b90ce163d66de3b84cdb7
VicZS <vicentezavsan@gmail.com>
2025-06-08 03:59:59 -0600

    Barra Superior OK

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            PanelSuperior = new Panel();
+            PanelNombreApp = new Panel();
+            label1 = new Label();
+            PanelControles = new Panel();
+            PBMinimizar = new PictureBox();
+            PBCerrar = new PictureBox();
+            PanelLogoForvia = new Panel();
+            PBLogoForvia = new PictureBox();
+            PanelSuperior.SuspendLayout();
+            PanelNombreApp.SuspendLayout();
+            PanelControles.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)PBMinimizar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)PBCerrar).BeginInit();
+            PanelLogoForvia.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)PBLogoForvia).BeginInit();
+            SuspendLayout();
+            // 
+            // PanelSuperior
+            // 
+            PanelSuperior.BackColor = Color.FromArgb(17, 47, 84);
+            PanelSuperior.Controls.Add(PanelNombreApp);
+            PanelSuperior.Controls.Add(PanelControles);
+            PanelSuperior.Controls.Add(PanelLogoForvia);
+            PanelSuperior.Dock = DockStyle.Top;
+            PanelSuperior.Location = new Point(0, 0);
+            PanelSuperior.Name = "PanelSuperior";
+            PanelSuperior.Padding = new Padding(10);
+            PanelSuperior.Size = new Size(1280, 66);
+            PanelSuperior.TabIndex = 0;
+            PanelSuperior.MouseDown += ArrastrarVentana;
+            // 
+            // PanelNombreApp
+            // 
+            PanelNombreApp.Controls.Add(label1);
+            PanelNombreApp.Dock = DockStyle.Left;
+            PanelNombreApp.Location = new Point(199, 10);
+            PanelNombreApp.Name = "PanelNombreApp";
+            PanelNombreApp.Padding = new Padding(1);
+            PanelNombreApp.Size = new Size(574, 46);
+            PanelNombreApp.TabIndex = 2;
+            PanelNombreApp.MouseDown += ArrastrarVentana;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Right;
+            label1.Font = new Font("Segoe UI Semibold", 24F, FontStyle.Bold);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(345, 1);
+            label1.Name = "label1";
+            label1.Size = new Size(228, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Tape Manager";
+            label1.MouseDown += ArrastrarVentana;
+            // 
+            // PanelControles
+            // 
+            PanelControles.Controls.Add(PBMinimizar);
+            PanelControles.Controls.Add(PBCerrar);
+            PanelControles.Dock = DockStyle.Right;
+            PanelControles.Location = new Point(1158, 10);
+            PanelControles.Name = "PanelControles";
+            PanelControles.Size = new Size(112, 46);
+            PanelControles.TabIndex = 1;
+            // 
+            // PBMinimizar
+            // 
+            PBMinimizar.Cursor = Cursors.Hand;
+            PBMinimizar.Dock = DockStyle.Right;
+            PBMinimizar.Image = Properties.Resources.minimizar;
+            PBMinimizar.Location = new Point(20, 0);
+            PBMinimizar.Name = "PBMinimizar";
+            PBMinimizar.Size = new Size(46, 46);
+            PBMinimizar.SizeMode = PictureBoxSizeMode.StretchImage;
+            PBMinimizar.TabIndex = 1;
+            PBMinimizar.TabStop = false;
+            PBMinimizar.Click += PBMinimizar_Click;
+            PBMinimizar.MouseEnter += PBMinimizar_MouseEnter;
+            PBMinimizar.MouseLeave += PBMinimizar_MouseLeave;
+            // 
+            // PBCerrar
+            // 
+            PBCerrar.Cursor = Cursors.Hand;
+            PBCerrar.Dock = DockStyle.Right;
+            PBCerrar.Image = Properties.Resources.boton_cerrar;
+            PBCerrar.Location = new Point(66, 0);
+            PBCerrar.Name = "PBCerrar";
+            PBCerrar.Size = new Size(46, 46);
+            PBCerrar.SizeMode = PictureBoxSizeMode.StretchImage;
+            PBCerrar.TabIndex = 0;
+            PBCerrar.TabStop = false;
+            PBCerrar.Click += PBCerrar_Click;
+            PBCerrar.MouseEnter += PBCerrar_MouseEnter;
+            PBCerrar.MouseLeave += PBCerrar_MouseLeave;
+            // 
+            // PanelLogoForvia
+            // 
+            PanelLogoForvia.Controls.Add(PBLogoForvia);
+            PanelLogoForvia.Dock = DockStyle.Left;
+            PanelLogoForvia.Location = new Point(10, 10);
+            PanelLogoForvia.Name = "PanelLogoForvia";
+            PanelLogoForvia.Padding = new Padding(0, 14, 0, 0);
+            PanelLogoForvia.Size = new Size(189, 46);
+            PanelLogoForvia.TabIndex = 0;
+            PanelLogoForvia.MouseDown += ArrastrarVentana;
+            // 
+            // PBLogoForvia
+            // 
+            PBLogoForvia.Dock = DockStyle.Fill;
+            PBLogoForvia.Image = Properties.Resources.Logo_forvia;
+            PBLogoForvia.Location = new Point(0, 14);
+            PBLogoForvia.Name = "PBLogoForvia";
+            PBLogoForvia.Size = new Size(189, 32);
+            PBLogoForvia.SizeMode = PictureBoxSizeMode.StretchImage;
+            PBLogoForvia.TabIndex = 0;
+            PBLogoForvia.TabStop = false;
+            PBLogoForvia.MouseDown += ArrastrarVentana; ;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1280, 720);
+            Controls.Add(PanelSuperior);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            PanelSuperior.ResumeLayout(false);
+            PanelNombreApp.ResumeLayout(false);
+            PanelNombreApp.PerformLayout();
+            PanelControles.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)PBMinimizar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)PBCerrar).EndInit();
+            PanelLogoForvia.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)PBLogoForvia).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel PanelSuperior;
+        private Panel PanelLogoForvia;
+        private PictureBox PBLogoForvia;
+        private Panel PanelControles;
+        private PictureBox PBCerrar;
+        private PictureBox PBMinimizar;
+        private Panel PanelNombreApp;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,7 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using System.Runtime.InteropServices;
+
+
 namespace TapeManager
 {
     public partial class Form1 : Form
@@ -6,5 +10,63 @@ namespace TapeManager
         {
             InitializeComponent();
         }
+
+        private void PBCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void PBCerrar_MouseEnter(object sender, EventArgs e)
+        {
+            PBCerrar.Image = Properties.Resources.boton_cerrar___rojo;
+        }
+
+        private void PBCerrar_MouseLeave(object sender, EventArgs e)
+        {
+            PBCerrar.Image = Properties.Resources.boton_cerrar;
+        }
+
+
+        private void PBMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void PBMinimizar_MouseEnter(object sender, EventArgs e)
+        {
+            PBMinimizar.Image = Properties.Resources.minimizar_Azul;
+        }
+
+        private void PBMinimizar_MouseLeave(object sender, EventArgs e)
+        {
+            PBMinimizar.Image = Properties.Resources.minimizar;
+        }
+
+        // Estas funciones permiten mover la ventana
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        // Constantes necesarias
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
+
+
+        private void ArrastrarVentana(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+
+
+
+
+
+
     }
 }
